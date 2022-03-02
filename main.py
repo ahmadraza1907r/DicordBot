cfc85fd49dce436116e6f4c636604e8ee91ef89b
ahmadraza1907r <95628522+ahmadraza1907r@users.noreply.github.com>
2022-03-02 21:33:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ userIDS = ["922517239251476491"]
 
 
 ForwardWebHook = "https://discord.com/api/webhooks/948442292052717608/_2WQ2sw_e5vz6Kh4_ZfMwFjlA9dv9zfshafp1-ehkeBIy6L1sxc57I2G3zYsZInDG3-h" # Here enter the webhook link in which you want to forward messages
-token = "OTIyNTE3MjM5MjUxNDc2NDkx.Yh-XHw.phNn7VlSQu9WGVf8QO5t-1WLm4s" # Here you add TOKEN of your account. Add you token in the double qouts
+token = "OTIyNTE3MjM5MjUxNDc2NDkx.Yh-YPQ.VoklmdcXL6U-jSiwxtusunLAyKo" # Here you add TOKEN of your account. Add you token in the double qouts
 
 #Don't change the below code
 
